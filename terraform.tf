7f0544b808fb412f3b97d6427abdd05770d38ee6
richards28 <richirocks96@gmail.com>
2025-04-04 21:54:54 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -56,10 +56,12 @@ resource "aws_iam_role" "ecs_task_execution_role" {
         Service = "ecs-tasks.amazonaws.com"
       }
     }]
+  })
+
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AdministratorAccess"
   ]
-})
+}
 
 resource "aws_ecs_task_definition" "medusa_task" {
   family                   = "medusa-task"
@@ -80,78 +82,5 @@ resource "aws_ecs_task_definition" "medusa_task" {
       hostPort      = 9000
     }]
     environment = [
-      { name = "DATABASE_URL", value = "postgres://medusa_user:medusa_pass@${aws_db_instance.medusa_db.endpoint}/medusa" },
-      { name = "REDIS_URL", value = "redis://redis:6379" }
-    ]
-  }])
-}
-
-resource "aws_ecs_service" "medusa_service" {
-  name            = "medusa-service"
-  cluster         = aws_ecs_cluster.medusa_cluster.id
-  task_definition = aws_ecs_task_definition.medusa_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = module.vpc.public_subnets
-    security_groups  = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
-}
-
-resource "aws_db_instance" "medusa_db" {
-  identifier        = "medusa-db"
-  engine           = "postgres"
-  instance_class   = "db.t3.micro"
-  allocated_storage = 20
-  username        = "medusa_user"
-  password        = "medusa_pass"
-  publicly_accessible = false
-  skip_final_snapshot = true
-}
-
-resource "aws_security_group" "medusa_sg" {
-  name        = "medusa-security-group"
-  description = "Allow HTTP, HTTPS, and Medusa API traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SSH (Restrict in production)
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # HTTP
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # HTTPS
-  }
-
-  ingress {
-    from_port   = 7000
-    to_port     = 7000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Medusa API
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "medusa_task_public_ip" {
-  value = aws_ecs_service.medusa_service.network_configuration[0].assign_public_ip
-}
-
+      { name = "DATABASE_URL", value = "postgres://medusa_user:medusa_pass@${aws_db_instance.medusa_db.address}/medusa" },
+      { name = "REDIS_URL", value = "redis://redis_

482cc71d353d83df7050dd47ba32d08b97f1a1e0
richards28 <richirocks96@gmail.com>
2025-04-04 21:47:57 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -13,7 +13,7 @@ module "vpc" {
   name = "medusa-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["ap-northeast-1a", "ap-northeast-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
